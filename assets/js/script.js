45799e057bbe0f9142ed67e5bb028b5c8b31aef1
Steph T <wytseng@uci.edu>
2022-11-16 13:30:42 -0800

    finish ending screen, add bootstrap, add questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    question: "What is not a way to declare a variable in JavaScript?",
+    choices: ["var", "let", "const", "list"],
+    answer: "list",
+  },
+  {
+    question: "What is the default behavior where all the variable and function declaration are moves to the top called?" ,
+    choices: ["bubbling", "debugging", "capturing", "hoisting"],
+    answer: "hoisting",
+  },
+  {
+    question: "What line must you include to prevent a form button from auto-refreshing the page.",
+    choices: ["event.preventDefault", "event.stopPropagration", "event.cancelBubble", "even.initEvent"],
+    answer: "event.preventDefault",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "javascript", "<scripting>", "<js>"],
+    answer: "<script>",
+  },
+  {
+    question: "How do you declare a function on JavaScript?",
+    choices: ["function myFunction()", "var myFunction = function()", "both works"],
+    answer: "both works",
+  },
+];
