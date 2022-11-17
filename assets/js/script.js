f7c399cbb48be03d3ebcf5cc73b6b72ac230bc6d
Steph T <wytseng@uci.edu>
2022-11-16 17:51:20 -0800

    add id on elements, refine css, working javascript (quiz portion)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Array of question-choices-answer objects
 var questions = [
   {
     question: "What is not a way to declare a variable in JavaScript?",
@@ -16,7 +17,7 @@ var questions = [
   },
   {
     question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<script>", "javascript", "<scripting>", "<js>"],
+    choices: ["\<script\>", "javascript", "<scripting>", "<js>"],
     answer: "<script>",
   },
   {
@@ -26,4 +27,113 @@ var questions = [
   },
 ];
 
+// gobal variables
+var questionEL = document.getElementById("question");
+var choicesEl = document.getElementById("choices");
+var timerEl = document.getElementById("time-count");
+var questionScreen = document.getElementById("question-screen");
 
+var totalTime = questions.length * 15;
+var currentIndex = 0;
+var timer;
+var highscores = [];
+
+// starts the quiz and timer 
+function startQuiz() {
+  var startScreen = document.getElementById("start-screen");
+
+  startScreen.setAttribute("class", "hide");
+  questionScreen.removeAttribute("class", "hide");
+
+  timerEl.textContent = totalTime;
+  startTimer();
+
+  renderQuestions();
+}
+
+// function handling the timer countdown 
+function startTimer() {
+  timer = setInterval( function() {
+    if (totalTime > 0) {
+      totalTime--;
+      timerEl.textContent = totalTime;
+    } else {
+      endGame();
+    }
+  }, 1000)
+}
+
+// displays the question at currentIndex 
+function renderQuestions() {
+  // clears the choices div if it is not empty 
+  if (choicesEl.childNodes.length !== 0) {
+    choicesEl.replaceChildren();
+  }
+
+  questionEL.innerHTML = questions[currentIndex].question;
+  document.getElementById("question-number").innerHTML = (currentIndex + 1);
+
+  var choices = questions[currentIndex].choices;
+  for (var i = 0; i < choices.length; i++) {
+    var choiceOption = choices[i];
+    var choiceOptionEl = document.createElement("button");
+    choiceOptionEl.setAttribute("class", "choice");
+    choiceOptionEl.setAttribute("value", choiceOption);
+    choiceOptionEl.setAttribute("id", "choice");
+    choiceOptionEl.innerHTML = (i+1) + ". " + choiceOption;
+    choicesEl.appendChild(choiceOptionEl);
+  }
+}
+
+// checks the answer 
+function checkAnswer(event) {
+  // check if the element triggering the click is a choice element
+  if (event.target.id === "choice") {
+    // checks if the answer is correct 
+    if (event.target.value === questions[currentIndex].answer) {
+      console.log(event.target.value + " = " + questions[currentIndex].answer);
+    } else {
+      console.log("question " + (currentIndex+1) + " is incorrect");
+      totalTime = totalTime - 15;
+    }
+  
+    if (totalTime <= 0 || currentIndex >= (questions.length-1)) {
+      endGame();
+    } else {
+      currentIndex++;
+      renderQuestions();
+    }
+  }
+}
+
+// displays the ending screen 
+function endGame() {
+  clearInterval(timer);
+  questionScreen.setAttribute("class", "hide");
+  document.getElementById("end-screen").removeAttribute("class", "hide");
+
+  document.getElementById("score").textContent = totalTime;
+}
+
+// Starts the quiz 
+document.getElementById("start-quiz").addEventListener("click", startQuiz);
+
+// Checks accuracy of the answer chosen
+document.getElementById("choices").addEventListener("click", checkAnswer);
+
+function saveHighscore() {
+  var initials = document.getElementById("initials").value.trim();
+
+  var newScore = {
+    initials: initials,
+    score: totalTime
+  };
+
+  highscores.push(newScore);
+  highscores = highscores.sort(function(a, b) {return a.score - b.score});
+  console.log(highscores);
+  localStorage.setItem("highscores", highscores);
+}
+
+// Submit score
+document.getElementById("submit").addEventListener("submit", saveHighscore);
