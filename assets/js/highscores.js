f5d6e4e8111cf7281743580cf8591e1d5a505ccc
Steph T <wytseng@uci.edu>
2022-11-16 22:17:19 -0800

    tidy up css file, add in detialed comments for all

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,13 +1,16 @@
-//  declare global highscores container
-var highscores = [];
+// Declare global highscores (array)
+var highscores;
 
-// inputs the list of highscores in order from highest to lowest
+// Inputs the list of highscores in order from highest to lowest
 function renderScoreboard() {
+  // Input local storage data, or assign empty array if empty
   highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
   if (highscores.length !== 0) {
     var scoreBoardEl = document.getElementById("scoreboard");
+    // Sort highscores from high to low
     highscores = highscores.sort(function(a, b) {return b.score - a.score});
+    // Inser list item for each highscore
     for (var i=0; i < highscores.length; i++) {
       let currentScore = highscores[i];
       var scoreEl = document.createElement("li");
@@ -18,18 +21,18 @@ function renderScoreboard() {
   }
 }
 
-// clears the local storage and the highscore variable 
+// Clears the local storage and the highscore variable 
 function clearHighscores() {
   if (highscores.length !== 0) {
     highscores = [];
     localStorage.removeItem('highscores');
   }
-  // refreshes the window to display the updated empty scoreboard
+  // Refreshes the window to display the updated empty scoreboard
   window.location.reload();
 }
 
-// clears local storage and regreshes page when clear button is clicked
+// Clears local storage and regreshes page when clear button is clicked
 document.getElementById("clear").addEventListener("click", clearHighscores);
 
-// run on page load
+// Init
 renderScoreboard();
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var questions = [
   },
   {
     question: "What line must you include to prevent a form button from auto-refreshing the page.",
-    choices: ["event.preventDefault", "event.stopPropagration", "event.cancelBubble", "even.initEvent"],
+    choices: ["event.stopPropagration", "event.preventDefault", "event.cancelBubble", "even.initEvent"],
     answer: "event.preventDefault",
   },
   {
@@ -27,96 +27,105 @@ var questions = [
   },
 ];
 
-// gobal variables
+// Assign global variable to DOM elements 
 var questionEL = document.getElementById("question");
 var choicesEl = document.getElementById("choices");
 var timerEl = document.getElementById("time-count");
 var answerSheetEl = document.getElementById("answer-sheet");
 var questionScreen = document.getElementById("question-screen");
 
+// Global variables 
 var totalTime = questions.length * 15;
 var currentIndex = 0;
 var answerCheck = [];
 var timer;
 
-// starts the quiz and timer 
+// Starts the quiz, stars the coundown and renders the first question
 function startQuiz() {
   var startScreen = document.getElementById("start-screen");
-
+  // Hide starting screen, show question screen
   startScreen.setAttribute("class", "hide");
   questionScreen.removeAttribute("class", "hide");
-
+  // Display timer time and start countdown
   timerEl.textContent = totalTime;
   startTimer();
-
+  // Display first question
   renderQuestions();
 }
 
-// function handling the timer countdown 
+// Handles timer countdown
 function startTimer() {
   timer = setInterval( function() {
     if (totalTime > 0) {
       totalTime--;
       timerEl.textContent = totalTime;
     } else {
-      endGame();
+      endQuiz();
     }
   }, 1000)
 }
 
+// Checks and inserts whether the previous answer was correct
 function renderAnswerCheck() {
+  // Ignore if no questions have been answered
   if (answerCheck.length !== 0) {
+    // Toggle answer sheet to show if first question has been answered
     if (answerCheck.length === 1) {
       answerSheetEl.removeAttribute("class", "hide");
     }
+    // Display whether the answer to previous question was right or wrong
     var answerCheckEl = document.createElement("p");
     answerCheckEl.innerHTML = "Question " + (answerCheck.length) + ": " + answerCheck.slice(-1);
     answerSheetEl.appendChild(answerCheckEl);
   }
 }
 
-// displays the question at currentIndex 
+// Displays a question at currentIndex 
 function renderQuestions() {
-  // clears the choices div if it is not empty 
+  // Clears the choices div if it is not empty 
   if (choicesEl.childNodes.length !== 0) {
     choicesEl.replaceChildren();
   }
-
+  // Display question prompt and update completion progress
   questionEL.innerHTML = questions[currentIndex].question;
-  document.getElementById("question-number").innerHTML = currentIndex;
-
+  document.getElementById("progress").innerHTML = currentIndex;
+  // Get array of choices for the responding prompt
   var choices = questions[currentIndex].choices;
+  // Loop through each choice and appen a button for the choice to the HTML
   for (var i = 0; i < choices.length; i++) {
     var choiceOption = choices[i];
     var choiceOptionEl = document.createElement("button");
     choiceOptionEl.setAttribute("class", "choice");
     choiceOptionEl.setAttribute("value", choiceOption);
-    choiceOptionEl.setAttribute("id", "choice");
     choiceOptionEl.innerHTML = (i+1) + ". " + choiceOption;
     choicesEl.appendChild(choiceOptionEl);
   }
+  // Displays the checked answer up to the previous question
   renderAnswerCheck();
 }
 
-// checks the answer 
+// Checks the chosen answer 
 function checkAnswer(event) {
-  // check if the element triggering the click is a choice element
-  if (event.target.id === "choice") {
-    // checks if the answer is correct 
+  // Checks if the element triggering the click is a choice class
+  if (event.target.className === "choice") {
+    // Checks if the answer is incorrect 
     if (event.target.value !== questions[currentIndex].answer) {
       totalTime -= 15;
       answerCheck.push("X");
+      // No time score can be negative
       if (totalTime < 0 ) {
         totalTime = 0;
       }
+      // Update the timer with the deducted time
+      timerEl.textContent = totalTime;
     } else {
       answerCheck.push("O");
     }
-
-    timerEl.textContent = totalTime;
-  
+    
+    // End quiz if ran out of time, or if all questions completed
+    // Else render the next question 
     if (totalTime <= 0 || currentIndex >= (questions.length-1)) {
-      endGame();
+      endQuiz();
     } else {
       currentIndex++;
       renderQuestions();
@@ -124,46 +133,51 @@ function checkAnswer(event) {
   }
 }
 
-// displays the ending screen 
-function endGame() {
+// Displays the ending screen and ends the quiz
+function endQuiz() {
+  // Stop timer countdown
   clearInterval(timer);
-
+  // Update answer sheet with correctness of all questions
   renderAnswerCheck();
+  // Show end screen, hide question screen
   questionScreen.setAttribute("class", "hide");
   document.getElementById("end-screen").removeAttribute("class", "hide");
-
+  // Show student the total score
   document.getElementById("score").textContent = totalTime;
 }
 
+// Trigger saveHighscore if Enter key is pressed in input
 function checkEnter(event) {
   if (event.key === "Enter") {
     saveHighscore();
   }
 }
 
+// Saves initials and score to local storage 
 function saveHighscore() {
   var initials = document.getElementById("initials").value.trim();
-
+  // Check if input is empty
   if (initials !== '') {
+    // Get highscores from local storage, declares array if no record
     var highscores = JSON.parse(localStorage.getItem("highscores")) || [] ;
-  
+    // Declare object with current initial and scores
     var newScore = {
       initials: initials,
       score: totalTime
     };
-  
+    
     highscores.push(newScore);
     localStorage.setItem("highscores", JSON.stringify(highscores));
+    // Redirect to highscores page
     window.location.href = "highscores.html";
   }
 }
 
 // Starts the quiz 
 document.getElementById("start-quiz").addEventListener("click", startQuiz);
-
 // Checks accuracy of the answer chosen
 document.getElementById("choices").addEventListener("click", checkAnswer);
-
-// Submit score
+// Submit score on button click
 document.getElementById("submit").addEventListener("click", saveHighscore);
+// Checks the key for a keyup event within the input field
 document.getElementById("initials").addEventListener("keyup", checkEnter);
